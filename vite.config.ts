368ffd050fbb8ea9d996c3aa4d15de0a7677486c
Your Name <p.kolby24@gmail.com>
2024-12-13 00:23:10 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/fan-sphere/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
 })
